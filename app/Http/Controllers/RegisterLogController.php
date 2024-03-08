bda3e6ee3b233003f6373f85f99747bf181b3f54
Kaue-Romero <kaueleiving@gmail.com>
2024-03-08 17:08:54 -0300

    feat: Creating the redirect and making the log

--- app/Http/Controllers/RegisterLogController.php
+++ app/Http/Controllers/RegisterLogController.php
@@ -2,27 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Register;
 use App\Models\RegisterLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class RegisterLogController extends Controller
 {
-    public function index(Request $request, $token)
+    public function index(Request $request, $redirect)
     {
-        return $this->logRequest($request, $token);
+        $requestParams = $request->query();
+        $requestString = '';
+        foreach ($requestParams as $key => $value) {
+            $value = htmlspecialchars($value);
+
+            $requestString .= $key . '=' . $value;
+            if ($key !== array_key_last($requestParams)) {
+                $requestString .= '&';
+            }
+        }
+
+        return $this->logRequest($request, $redirect, $requestString);
     }
 
-    private function logRequest(Request $request, string $token)
+    public function stats(Request $_request, $redirect)
     {
+        $logs = Register::where('code', $redirect)->with('logs')->get();
+
+        $total = count($logs[0]->logs);
+        $unique = count($logs[0]->logs->groupBy('ip'));
+
+        $access = ['total' => $total, 'uniques' => $unique];
+
+        $logs[0]->redirects = $access;
+        $logs[0]->top_referer = $logs[0]->logs->groupBy('header')->map(function ($item, $key) {
+            return [
+                'header' => $key,
+                'count' => count($item)
+            ];
+        })->sortDesc()->values()->first();
+
+        return response()->json($logs);
+    }
+
+    private function logRequest(Request $request, string $token, string $requestString)
+    {
+        $redirect = Register::where('code', $token)->first();
+
+        if (!$redirect) {
+            return response()->json(['error' => 'Invalid token'], 404);
+        }
+
         $log = new RegisterLog();
-        $log->id = $token;
         $log->ip = $request->ip();
         $log->user_agent = $request->userAgent();
-        //Referrer from header
         $log->header = $request->header('referer');
-        $log->query_params = json_encode($request->query());
+        $log->query_params = $requestString;
+        $log->redirect_id = $redirect->id;
+        $log->save();
 
-        dd($log);
-        //$log->save();
+        $destination = $this->checkForExistantQueryString($redirect->url, $requestString);
+
+        return Redirect::to($destination);
+    }
+
+    private function checkForExistantQueryString($queryString, $requestString)
+    {
+        if (strpos($queryString, '?') !== false) {
+            if (substr($queryString, -1) !== '?') {
+                $queryString .= '&' . $requestString;
+            } else {
+                $queryString .= $requestString;
+            }
+        } else {
+            $queryString .= '?' . $requestString;
+        }
+
+        return $queryString;
     }
 }
